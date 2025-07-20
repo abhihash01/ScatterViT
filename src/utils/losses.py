d73d21ab30708b063c45fccb700ed0df9aa751c6
abhihash01 <abhilash29998@gmail.com>
2025-07-20 19:57:08 -0400

    implemented focal loss

---
+++ src/utils/losses.py
@@ -0,0 +1,27 @@
+# src/utils/losses.py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FocalLoss(nn.Module):
+    """Focal Loss for multi-label classification"""
+    def __init__(self, alpha=1, gamma=2, reduction='mean'):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+    
+    
+    
+    def forward(self, inputs, targets):
+        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
+        
+        
+        
+        if self.reduction == 'mean':
+            return focal_loss.mean()
+        elif self.reduction == 'sum':
+            return focal_loss.sum()
+        return focal_loss
